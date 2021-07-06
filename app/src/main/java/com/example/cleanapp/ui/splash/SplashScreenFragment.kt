0af7152fa6c50653a8eee6792f0a80bb8f51c49b
enbok <en.bokuchava@gmail.com>
2021-07-07 00:22:00 +0400

    not to consider

--- app/src/main/java/com/example/cleanapp/ui/splash/SplashScreenFragment.kt
+++ app/src/main/java/com/example/cleanapp/ui/splash/SplashScreenFragment.kt
@@ -1,5 +1,6 @@
 package com.example.cleanapp.ui.splash
 
+import android.animation.ValueAnimator
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.cleanapp.R
@@ -8,22 +9,24 @@ import com.example.cleanapp.databinding.SplashScreenFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SplashScreenFragment : BaseFragment<SplashScreenFragmentBinding>(SplashScreenFragmentBinding::inflate) {
+class SplashScreenFragment :
+    BaseFragment<SplashScreenFragmentBinding>(SplashScreenFragmentBinding::inflate) {
 
-    private val viewModel:SplashScreenViewModel by viewModels()
+    private val viewModel: SplashScreenViewModel by viewModels()
 
     override fun start() {
+//        initAnim()
         observes()
         viewModel.checkAuth()
+
     }
 
-    private fun observes(){
-        viewModel.liveData.observe(viewLifecycleOwner,{
-            if(it)
-                findNavController().navigate(R.id.action_splashScreenFragment_to_chooserFragment)
+    private fun observes() {
+        viewModel.liveData.observe(viewLifecycleOwner, {
+            if (it)
+                findNavController().navigate(R.id.action_splashScreenFragment_to_chooserFragment2)
             else
-                findNavController().navigate(R.id.action_splashScreenFragment_to_SignInFragment)
+                findNavController().navigate(R.id.action_splashScreenFragment_to_chooserFragment2)
         })
     }
-
 }
